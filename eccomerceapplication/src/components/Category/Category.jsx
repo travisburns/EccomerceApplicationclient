249b88c63ee27389247b47f9fdd5b1e224625062
Travis <travisjb0@gmail.com>
2023-11-24 17:02:20 -0800

    Added category items fetch to home page

--- eccomerceapplication/src/components/Category/Category.jsx
+++ eccomerceapplication/src/components/Category/Category.jsx
@@ -9,6 +9,9 @@ const Category = ({ categories, status, }) => {
     if(status === STATUS.Error) return (<Error />)
     if(status === STATUS.LOADING) return (<Loader />)
 
+
+    console.log(categories);
+
   return (
     <section className='categories py-5 bg-ghost-white' id = 'categories'>
         <div className='container'>
@@ -16,6 +19,24 @@ const Category = ({ categories, status, }) => {
                 <div className='section-title'>
                     <h3 className='text-uppercase fw-7 text-regal-blue ls-1'>Category</h3>
                 </div>
+
+                <div className='category-items grid'>
+                    {
+                        categories.slice(0, 5).map(category =>  (
+                            <Link to={`cateogry/${category.id}`} key = {categories.id}>
+                                <div className='category-item'>
+                                    <div className='category-item-img'>
+                                        <img src = {category.image} alt= "" />
+                                    </div>
+                                    <div className='category-item-name text-center'>
+                                        <h6 className='fs-20'>{category.name}</h6>
+                                    </div>
+                                </div>
+                            </Link>
+                        ))
+                    }
+                </div>
+
             </div>
         </div>
     </section>
